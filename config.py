bdfcce15f02caed1292d1fb5a89bca3fe27a47a3
Nyankz <zhakefigmm@gmail.com>
2025-06-30 23:17:53 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,37 @@
+#Трогать, значения можно менять
+admin = [764515145] #ID админа
+support_name = '@renzokz' #Имя саппорта, будет выводиться при нажатии кнопки "поддержка"
+token = '7684111869:AAFMIAEfA9oLaknk5GUYBDrpgr1uk-HNLZU' #Токен бота
+client_id = "" #client_id, получается при создании приложения Юмани
+number_qiwi = '00000000' #Номер вашего QIWI кошелька
+token_qiwi = '' #Токен киви
+token_youmoney = ''  # Токен ЮМани
+p2p_closed_token =''  #Приватный ключ P2P QIWI
+open_coinbase = '' #Публичный ключ CoinBase
+close_coinbase = '' #Секретный токен CoinBase
+secret_crystal = ''  #Секретный ключ кассы CrystalPay
+login_crystal = ''  # Логин кассы CrystalPay
+
+#Не трогать, значения должны быть по умолчанию
+balance = 0
+referals_ = 0
+bill_id =  None
+photo_id = None
+text = None
+auto_output = 0
+amount = None
+bet = 0
+game = None
+referal_level = 1
+referer = None
+referal_profit = 0
+ban = 0
+cash = 0
+method = None
+voucher = None
+promo = None
+winning = 0
+demobalance = 0
+
+
+
